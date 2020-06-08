596e86258c2e6b69cca11df0fb8b97c6c8cc3f14
KKambi <37494867+KKambi@users.noreply.github.com>
2020-06-08 23:16:45 +0900

    Add: findAll()이 Iterable<T>가 아닌 List<T>를 반환하기 위한 오버라이딩

--- src/main/java/org/kkambi/PackUmbrella/domain/forecast/ForecastRepository.java
+++ src/main/java/org/kkambi/PackUmbrella/domain/forecast/ForecastRepository.java
@@ -2,6 +2,10 @@ package org.kkambi.PackUmbrella.domain.forecast;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ForecastRepository extends CrudRepository<Forecast, String> {
 
+    @Override
+    List<Forecast> findAll();
 }
