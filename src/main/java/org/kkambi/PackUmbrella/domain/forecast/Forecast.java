91fd098af7685814fd5e5e4b3dfc7f2cf234b7d1
KKambi <37494867+KKambi@users.noreply.github.com>
2020-06-08 23:07:50 +0900

    Add: 도메인 클래스 빌더 패턴 적용

--- src/main/java/org/kkambi/PackUmbrella/domain/forecast/Forecast.java
+++ src/main/java/org/kkambi/PackUmbrella/domain/forecast/Forecast.java
@@ -15,6 +15,39 @@ public class Forecast {
 
     private String T3H;     //3시간 기온
 
+    public static class Builder {
+        private String POP;     //강수확률
+
+        private String PTY;     //강수형태
+
+        private String T3H;     //3시간 기온
+
+        public Builder pop(String pop) {
+            this.POP = pop;
+            return this;
+        }
+
+        public Builder pty(String pty) {
+            this.PTY = pty;
+            return this;
+        }
+
+        public Builder t3h(String t3h) {
+            this.T3H = t3h;
+            return this;
+        }
+
+        public Forecast build() {
+            return new Forecast(this);
+        }
+    }
+
+    private Forecast(Builder builder) {
+        this.POP = builder.POP;
+        this.PTY = builder.PTY;
+        this.T3H = builder.T3H;
+    }
+
     public String getPOP() {
         return POP;
     }
