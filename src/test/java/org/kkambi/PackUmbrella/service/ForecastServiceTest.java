0814d5bee6458642ed3ffd8c622d88172209e4f0
KKambi <37494867+KKambi@users.noreply.github.com>
2020-07-10 15:09:08 +0900

    Add: forecast isCached func test

--- src/test/java/org/kkambi/PackUmbrella/service/ForecastServiceTest.java
+++ src/test/java/org/kkambi/PackUmbrella/service/ForecastServiceTest.java
@@ -2,6 +2,8 @@ package org.kkambi.PackUmbrella.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kkambi.PackUmbrella.domain.forecast.Forecast;
+import org.kkambi.PackUmbrella.domain.forecast.ForecastRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,6 +18,9 @@ public class ForecastServiceTest {
 
     @Autowired
     private ForecastService forecastService;
+
+    @Autowired
+    private ForecastRepository forecastRepository;
 
     @Test
     public void test_getMainDateTime() {
@@ -42,5 +47,36 @@ public class ForecastServiceTest {
         assertThat(mainDateTime.getDayOfMonth()).isEqualTo(1);
         assertThat(mainDateTime.getHour()).isEqualTo(6);
         assertThat(forecastDateTime.getHour()).isEqualTo(23);
+    }
+
+    @Test
+    public void test_when_forecast_is_cached() {
+        //given
+        LocalDateTime currDateTime = LocalDateTime.of(2020, 7, 31, 23, 42, 23);
+
+        forecastRepository.save(new Forecast.Builder()
+                                    .forecastDateTime(currDateTime)
+                                    .pop("80")
+                                    .pty("80")
+                                    .t3h("80")
+                                    .build());
+
+        //when
+        boolean result = forecastService.isForecastCached(currDateTime);
+
+        //then
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    public void test_when_forecast_is_not_cached() {
+        //given
+        LocalDateTime currDateTime = LocalDateTime.of(2020, 7, 31, 23, 42, 23);
+
+        //when
+        boolean result = forecastService.isForecastCached(currDateTime);
+
+        //then
+        assertThat(result).isEqualTo(false);
     }
 }
